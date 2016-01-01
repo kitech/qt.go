3a9f457f77d9ff7233753a1ff4d45fbb573e28b9
kitech <kitech@users.sf.net>
2016-01-02 01:00:59 +0800

    inited.

---
+++ src/qtrt/qtrt.go
@@ -0,0 +1,85 @@
+package qtrt
+
+import (
+	"fmt"
+	"reflect"
+	"unsafe"
+)
+
+func KeepMe() {}
+
+func SymbolResolve(args []interface{}, vtys map[int32]map[int32]reflect.Type) int32 {
+	return 0
+}
+
+func ErrorResolve(class, method string, args []interface{}) {
+	fmt.Println(class, method, args)
+}
+
+func BoolTy(pointer bool) reflect.Type {
+	if pointer {
+		var v = true
+		return reflect.TypeOf(&v)
+	}
+	return reflect.TypeOf(true)
+}
+
+func ByteTy(pointer bool) reflect.Type {
+	if pointer {
+		var v = byte(0)
+		return reflect.TypeOf(&v)
+	}
+	return reflect.TypeOf(byte(0))
+}
+
+func StringTy(pointer bool) reflect.Type {
+	var s = "foo"
+	if pointer {
+		return reflect.TypeOf(&s)
+	}
+	return reflect.TypeOf(s)
+}
+
+func Int16Ty(pointer bool) reflect.Type {
+	if pointer {
+		var v = uint16(0)
+		return reflect.TypeOf(&v)
+	}
+	return reflect.TypeOf(uint16(0))
+}
+
+func Int32Ty(pointer bool) reflect.Type {
+	if pointer {
+		var v = uint32(0)
+		return reflect.TypeOf(&v)
+	}
+	return reflect.TypeOf(uint32(0))
+}
+
+func Int64Ty(pointer bool) reflect.Type {
+	if pointer {
+		var v = uint64(0)
+		return reflect.TypeOf(&v)
+	}
+	return reflect.TypeOf(uint64(0))
+}
+
+func FloatTy(pointer bool) reflect.Type {
+	if pointer {
+		var v = float32(0.0)
+		return reflect.TypeOf(&v)
+	}
+	return reflect.TypeOf(float32(0.0))
+}
+func DoubleTy(pointer bool) reflect.Type {
+	if pointer {
+		var v = float64(0.0)
+		return reflect.TypeOf(&v)
+	}
+	return reflect.TypeOf(float64(0.0))
+}
+
+func VoidpTy() reflect.Type {
+	var v unsafe.Pointer = nil
+	return reflect.TypeOf(v)
+}
